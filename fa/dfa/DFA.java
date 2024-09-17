c11c7ceb8f31ffd52cae66aa8a14056ba9cc5b1d
Emily Thelander <emilythelander@u.boisestate.edu>
2024-09-16 22:31:21 -0600

    started remaining methods, modified some, added javadocs

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -4,6 +4,13 @@ import java.util.*;
 
 import fa.State;
 
+/**
+ * The DFA class, which implements DFAInterface, is responsible for creating and
+ * modifying the deterministic finite automata objects.
+ * 
+ * @author Emily Thelander
+ * @author Spencer Pattillo
+ */
 public class DFA implements DFAInterface {
 
     LinkedHashSet<Character> sigma;
@@ -12,38 +19,47 @@ public class DFA implements DFAInterface {
     LinkedHashSet<DFAState> finalStates;
     LinkedHashMap<DFAState, LinkedHashSet<DFAState>> transitionTable;
 
+    /**
+     * DFA constructor. Creates DFA objects and instantiates it with empty values.
+     */
     public DFA() {
 
-    sigma = new LinkedHashSet<>();
-    states = new LinkedHashSet<>();
-    startState = "";
-    finalStates = new LinkedHashSet<>();
-    transitionTable = new LinkedHashMap<>();
-
+        sigma = new LinkedHashSet<>();
+        states = new LinkedHashSet<>();
+        startState = "";
+        finalStates = new LinkedHashSet<>();
+        transitionTable = new LinkedHashMap<>();
     }
 
     @Override
     public boolean addState(String name) {
+
+        for (DFAState state : states) {
+            if (state.getName().equals(name)) {
+                return false;
+            }
+        }
 
         return states.add(new DFAState(name));
     }
 
     @Override
     public boolean setFinal(String name) {
+
         for (DFAState state : states) {
             if (state.getName().equals(name)) {
-                states.remove(state);
                 return finalStates.add(state);
             }
         }
+
         return false;
     }
 
     @Override
     public boolean setStart(String name) {
+
         for (DFAState state : states) {
             if (state.getName().equals(name)) {
-                states.remove(state);
                 startState = name;
                 return true;
             }
@@ -53,13 +69,31 @@ public class DFA implements DFAInterface {
 
     @Override
     public void addSigma(char symbol) {
+
+        for (Character alnum : sigma) {
+            if (alnum.equals(symbol)) {
+                return;
+            }
+        }
         sigma.add(symbol);
     }
 
     @Override
     public boolean accepts(String s) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accepts'");
+        // TODO Still incomplete
+        for (int i = 0; i < s.length(); i++) {
+            if (!sigma.contains(s.charAt(i))) {
+                return false;
+            }
+        }
+
+        if (startState == "") {
+            return false;
+        }
+
+        //Do another loop through the string
+        //Need to 
+        return true;
     }
 
     @Override
@@ -69,13 +103,13 @@ public class DFA implements DFAInterface {
 
     @Override
     public State getState(String name) {
-        for (State state : states){
-            if (state.getName().equals(name)){
+        for (State state : states) {
+            if (state.getName().equals(name)) {
                 return state;
             }
         }
-        for (State state : finalStates){
-            if (state.getName().equals(name)){
+        for (State state : finalStates) {
+            if (state.getName().equals(name)) {
                 return state;
             }
         }
@@ -85,15 +119,17 @@ public class DFA implements DFAInterface {
 
     @Override
     public boolean isFinal(String name) {
-        if (finalStates.contains(name)){
-            return true;
+        for (DFAState state : finalStates) {
+            if (state.getName().equals(name)) {
+                return true;
+            }
         }
         return false;
     }
 
     @Override
     public boolean isStart(String name) {
-        if (name.equals(startState)){
+        if (name.equals(startState)) {
             return true;
         }
         return false;
@@ -101,8 +137,33 @@ public class DFA implements DFAInterface {
 
     @Override
     public boolean addTransition(String fromState, String toState, char onSymb) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addTransition'");
+        // TODO Still incomplete
+
+        //Check that onSymb is in sigma
+        if (!sigma.contains(onSymb)) {
+            return false;
+        }
+
+        //There's probably a more efficient/better way to do these checks but I'm exhausted
+        boolean fromExists = false;
+        boolean toExists = false;
+        for (DFAState state : states) {
+            if (state.getName().equals(fromState)) {
+                fromExists = true;
+            } 
+        }
+
+        for (DFAState state : states) {
+            if (state.getName().equals(toState)) {
+                toExists = true;
+            } 
+        }
+
+        if (!fromExists || !toExists) {
+            return false;
+        }
+
+        return true;
     }
 
     @Override
